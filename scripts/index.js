0868fc6f86b74c67790dafd7b7f4ce3d3e7bf80f
Anton Ivanov <ivanov.businesstaxi@yandex.ru>
2020-07-12 04:22:34 +0300

    проектная работа №4 исправлены ошибки по первому ревью

--- scripts/index.js
+++ scripts/index.js
@@ -8,22 +8,26 @@ const occupationInput = formElement.querySelector('.popup__input_type_occupation
 const nameProfile = document.querySelector('.profile__name');
 const occupationProfile = document.querySelector('.profile__occupation');
 
-function togglePopup() {
-    popup.classList.toggle('popup_opened');    
+function togglePopup() {    
+     popup.classList.toggle('popup_opened');     
 }
-
-openPopupButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-
     nameProfile.textContent =  nameInput.value;
     occupationProfile.textContent = occupationInput.value;
+    togglePopup();
 }
 
+//я так понял, что toogle сам проверят есть ли класс или нет, а ошибка заключалась в том, что не заносились данные в форму при открытии попапа, но так как значения value у меня были прописаны в html этого момента я не заметил.// 
+openPopupButton.addEventListener('click', () => {
+    nameInput.value = nameProfile.textContent;
+    occupationInput.value = occupationProfile.textContent;
+    togglePopup();}
+);
+closePopupButton.addEventListener('click', togglePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-popupSubmit.addEventListener('click', togglePopup);
+
 
 
 
